b162831af15bd7760194aa8a352252fa3f892d81
htmmd27 <2394947535@qq.com>
2019-05-15 11:35:12 +0800

    数据库操作类与测试类

---
+++ src/main/java/com/example/demo/tool/SqlOperation.java
@@ -0,0 +1,8 @@
+package com.example.demo.tool;
+//存储所有对sql进行的操作函数
+
+
+public class SqlOperation {
+//定义数据库的连接
+
+}
---
+++ src/test/java/com/example/demo/tool/SqlOperationTest.java
@@ -0,0 +1,7 @@
+package com.example.demo.tool;
+
+import static org.testng.Assert.*;
+
+public class SqlOperationTest {
+
+}
