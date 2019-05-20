2d7a3defae3f12548472a4182a425dbd6eaefb33
liuyuesen <1301913704@qq.com>
2019-05-20 16:28:40 +0800

    在LoginControllerTest当中使用了assertJ进行测试

--- src/test/java/com/example/demo/Login/LoginControllerTest.java
+++ src/test/java/com/example/demo/Login/LoginControllerTest.java
@@ -6,16 +6,15 @@ import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
+import static org.assertj.core.api.Assertions.*;
+
 
 @ContextConfiguration(classes = SQLOperation.class)
 public class LoginControllerTest  extends AbstractTestNGSpringContextTests {
     LoginController loginController = new LoginController();
     @Test
-    public void testLogin() throws Exception {
+    public void testLoginRight() throws Exception {
         assertEquals(loginController.login(new LoginRequest("1301913704","980508Izwh")).equals( new LoginResponse(true,null)),true);
-        assertEquals(loginController.login(new LoginRequest("123456","123456")).equals(new LoginResponse(false,"账号不存在")), true);
-        assertEquals(loginController.login(new LoginRequest("1301913704","123456")).equals(new LoginResponse(false,"密码错误")), true);
-        assertEquals(loginController.login(new LoginRequest("123456","123456")).equals(new LoginResponse(false,"账号不存在")), true);
     }
 
     @Test
@@ -25,6 +24,7 @@ public class LoginControllerTest  extends AbstractTestNGSpringContextTests {
 
     @Test
     public void testLoginAccountFall () throws Exception{
-        assertEquals(loginController.login(new LoginRequest("123456","123456")).equals(new LoginResponse(false,"账号不存在")), true);
+//        assertEquals(loginController.login(new LoginRequest("123456","123456")).equals(new LoginResponse(false,"账号不存在")), true);
+        assertThat(loginController.login(new LoginRequest("123456","123456"))).isEqualToComparingFieldByFieldRecursively(new LoginResponse(false,"账号不存在"));
     }
 }
