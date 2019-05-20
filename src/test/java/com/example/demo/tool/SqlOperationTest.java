13ff2a135dd6d2eb86032d382ea8a020f0a8359b
liuyuesen <1301913704@qq.com>
2019-05-20 16:59:02 +0800

    添加了reportNG

--- src/test/java/com/example/demo/tool/SqlOperationTest.java
+++ src/test/java/com/example/demo/tool/SqlOperationTest.java
@@ -5,8 +5,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 
 import static org.testng.Assert.*;
+import static org.assertj.core.api.Assertions.*;
 
 @ContextConfiguration(classes = SQLOperation.class)
 public class SqlOperationTest extends AbstractTestNGSpringContextTests {
+//    Source source = new Source("jdbc:mysql://172.27.1.219:3306/dango?useUnicode=yes&amp;characterEncoding=UTF8&zeroDateTimeBehavior=convertToNull", "write", "!@#$1234%^&*5678ABCDabcd");
 
 }
